4a2d43b5c88d81cf6dfe987471a7a26895bc1f9a
robinanbu <robinanbu23@gmail.com>
2024-10-08 09:13:59 +0530

    for deployment

--- src/services/Api.js
+++ src/services/Api.js
@@ -1,21 +1,16 @@
-import axios from 'axios';
+import axios from "axios";
+import { server_API } from "../server";
 
 // API helper function to send form data to the backend
 export const requestBlood = async (formData) => {
-    try {
-        const response = await axios.post('https://blood-report-server.onrender.com/api/blood-request', formData);
-        return response.data;
-    } catch (error) {
-        console.error('Error submitting form:', error);
-        throw error;
-    }
+  try {
+    const response = await axios.post(
+      `${server_API}/post-blood-data`,
+      formData
+    );
+    return response.data;
+  } catch (error) {
+    console.error("Error submitting form:", error);
+    throw error;
+  }
 };
-// export const updateRequestStatus = async (id, status) => {
-//     try {
-//         const response = await axios.put(`http://localhost:3333/api/blood-request/update/${id}`, { status });
-//         return response.data;
-//     } catch (error) {
-//         console.error(`Error updating request ${id}:`, error);
-//         throw error;
-//     }
-// };
